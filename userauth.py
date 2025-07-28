17cc9b3cad97817131e8ae98f63699287ab45811
olivethotokunefor <olivethotokunefor@gmail.com>
2025-07-28 15:39:58 -0700

    second commit

--- userauth.py
+++ userauth.py
@@ -1,9 +1,9 @@
-# email= "oliveth@gmail.com"
-# password = "1234"
+email= "oliveth@gmail.com"
+password = "1234"
 
-# em = input("Enter your email: ")
-# pas = input("Enter your password: ")
+em = input("Enter your email: ")
+pas = input("Enter your password: ")
 
-# if em.lower() == email and pas.lower() == password:
-#     print("Welcome User!")
-# elif em.lower() == email 
+if em.lower() == email and pas.lower() == password:
+    print("Welcome User!")
+elif em.lower() == email 
